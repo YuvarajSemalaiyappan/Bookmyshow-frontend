308d14220a18727a84671931e709571466bf7439
YuvarajSemalaiyappan <yuaaraj@gmail.com>
2023-09-15 17:22:21 +0530

    updated

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
 export const config = {
-  api: "http://localhost:5000",
-  // api: "https://bookmyshow-backend-fsuo.onrender.com",
+  // api: "http://localhost:5000",
+  api: "https://bookmyshow-backend-blxv.onrender.com",
 };
